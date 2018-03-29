ac60fdedca55bffdb91bac3a322325acf08679b8
Ruochen Lin <ruochenlin@outlook.com>
2018-03-28 20:42:35 -0500

    Added matlab code

---
+++ as4/matlab/symm_lu.m
@@ -0,0 +1,16 @@
+function [L,U] = symm_lr(A)
+% Pre: A is a real symmetric matrix
+% Post: L, U are the resulting matrices of LU-factorization of A
+m = length(A);
+L = eye(m);
+for i = 1 : m - 1
+	for j = i + 1 : m
+		L(j, i) = A(j, i) / A(i, i);
+		A(j, i) = 0;
+		A(j, i + 1 : j) = A(j, i + 1 : j) - L(j, i) * A(i, i + 1 : j);
+		if i < j - 1 
+			A(i + 1 : j - 1, j) = A(j, i + 1 : j - 1)';
+		end
+	end
+end
+U = A;
